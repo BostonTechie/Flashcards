5449e9a9870658e2a6a41b1a7c1f0bd91d100607
andrewurq <andrewurq@gmail.com>
2021-11-10 12:49:40 -0500

    first front card v2

--- app.js
+++ app.js
@@ -8,10 +8,27 @@ class deck {
     addCard (card){
         this.cards.push(card)
     }
+  
+    nextCard(javaDeck){
+       
 
- 
+        //why isn't this working?
+        if ((i-2) >= this.cards.length){
+            window.alert('Congrats you reached the end of the deck')
+        }
+        
+        else {
+            i = ++i
+            $flashCard.text(this.cards[i].frontCard)
+            
+        }
+    }
+
+    flipCard (javaDeck){
+         console.log(this.cards[0].frontCard)
+    }
+    
 }
-
 
 
 const javaDeck = new deck ('javascript')
@@ -24,8 +41,17 @@ javaDeck.addCard(card5)
 // javaDeck.cards.forEach( (card)=> {addCard(card)})
 
 let $flashCard =  $('#flashCard')
+let i = 0
+
+
 $flashCard.text(javaDeck.cards[0].frontCard)
-console.log(javaDeck);
 
-javaDeck.cards.forEach( card=> console.log('here is your front of card ',card.frontCard,'and here is your back of card ',card.backCard))
 
+
+$('#flipCard').on('click',()=> javaDeck.flipCard())
+$('#nextCard').on('click',()=> javaDeck.nextCard())
+
+
+// javaDeck.cards.forEach( card=> console.log('here is your front of card ',card.frontCard,'and here is your back of card ',card.backCard))
+
+// console.log(javaDeck)
