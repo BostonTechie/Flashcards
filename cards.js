a35ec3747766c1abbf25c1bfcc626558571ac2db
andrewurq <andrewurq@gmail.com>
2021-11-17 13:49:37 -0500

    40 cards added

--- cards.js
+++ cards.js
@@ -8,8 +8,43 @@ class card {
 }
 
 
-const card1 = new card("What does HTML stand for?", "Hyper Text Markup Language");
+const card1 = new card("What does HTML stand for?", "Hyper Text Markup Language")
 const card2 = new card("What do <h1> through <h6> do?", "Creates heading text, with <h1> being the largest and <h6> is the smallest" );
-const card3 = new card("What does the <p> tag do??", "Creates a paragraph of text.");
-const card4 = new card("The <ul> element does what?", "Begins an “unordered list” (e.g., bullets).");
-const card5 = new card("The <li> element does what?", "Indicates each list item inside of a <ul>.");
+const card3 = new card("What does the <p> tag do??", "Creates a paragraph of text.")
+const card4 = new card("The <ul> element does what?", "Begins an “unordered list” (e.g., bullets).")
+const card5 = new card("The <li> element does what?", "Indicates each list item inside of a <ul>.")
+const card6 = new card("What are the only 3 languages that browsers can understand?","HTML, CSS, and JavaScript")
+const card7 = new card("What does css stand for?","Cascading Style Sheets")
+const card8 = new card("What does html express?","HTML helps browsers interpret the structure and semantics of a document in plain text.")
+const card9 = new card("What are tags what do they denote?","The parts of an HTML document are called elements and they are denoted with tags. Tags come at the beginning and end of an element's content. <h1> or <p> for example")
+const card10 = new card("What does the head section of html do?","The head element holds metadata about the document; metadata meaning extra information about the document beyond the content of the document.")
+const card11 = new card("What does the title do in the head section in HTML?","One required piece of metadata is the title element. Every page is required to have a title; without one the HTML document is invalid. The title element defines what shows up in the browser window, what the page is called when added to favorites/bookmarks, and what the page is titled in search-engine results.he head element holds metadata about the document; metadata meaning extra information about the document beyond the content of the document.")
+const card12 = new card("which section is used for text and tags that are shown directly on your webpage?","body")
+const card13 = new card("why should you add alternative text to your images?","So  the users can get an idea of what the image is before it loads, and  for the visually impaired")
+const card14 = new card("What is used to store information usually relevant to browsers and search engines?","Metatags")
+const card15 = new card("Why should you specify a background color if you are using an image for the actual background of your page ?","The background color will be shown until the image loads")
+const card16 = new card("What tag tells the browser where the page starts and stops ?","< html >")
+const card17 = new card("Imagelinks can show a text label if you add which property?","alt")
+const card18 = new card("Defining clickable sub-areas on an image is called?","imagemapping")
+const card19 = new card("Which two meta tags have special relevance for search engines ?","Description and Keywords")
+const card20 = new card("All normal webpages consist of what two parts ?","Head and body")
+const card21 = new card("Where in an HTML document is the correct place to refer to an external style sheet?","In the < head > section")
+const card22 = new card("A Web document is broken into sections. What are the tags called that create these sections?","Structure tags")
+const card23 = new card("How do you insert a comment in a CSS file?","/* this is a comment */")
+const card24 = new card("Which CSS property controls the text size?","font-size")
+const card25 = new card("When creating a Web document, what format is used to express an image's height and width?","Pixels")
+const card26 = new card("How do you display hyperlinks without an underline?","a {text-decoration:none}")
+const card27 = new card("How do you make the text bold In CSS?","font-weight:bold")
+const card28 = new card("What does TROUBLE stand for in term of boarder spacing in CSS?","Top, right, bottom, left …example  border-width:10px 1px 5px 20px")
+const card29 = new card("What attribute is used to specify number of rows?","Rowspan")
+const card30 = new card("How do you change the left margin of an element?","margin-left:")
+const card31 = new card("To change the size of an image in HTML use what ?","height and width")
+const card32 = new card("How do you make a list that lists its items with squares?","list-style-type: square")
+const card33 = new card("What does XHTML stand for?","Extensible Hypertext Markup Language")
+const card34 = new card("What is frame in HTML?","HTML Frames are used to divide the web browser window into multiple sections where each section can be loaded separately. ")
+const card35 = new card("Besides, another way to make text bold is what ?","< strong >")
+const card36 = new card("How many characters can be written with 1 Kilobyte ?","1024")
+const card37 = new card("What tag can add a space between elements on your page?","<br>  (break)")
+const card38 = new card("How can you include an external JavaScript file?","/script src='myfile.js'/")
+const card39 = new card("True/false: NaN == NaN","false.")
+const card40 = new card("What is a \"unary\" operator?","only operates on one value, like ++ or -- as in ++var.")
